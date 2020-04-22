8377d80e4f6b79dd1f3420a32bba46dc0958a852
lladdy <randallboland@bigpond.com>
2020-04-22 17:49:49 +0930

    Remove unused command line args

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ from sc2.protocol import ConnectionAlreadyClosed
 
 
 # Run ladder game
-# This lets python-sc2 connect to a LadderManager game: https://github.com/Cryptyc/Sc2LadderServer
+# This lets python-sc2 connect to a ladder game.
 # Based on: https://github.com/Dentosal/python-sc2/blob/master/examples/run_external.py
 def run_ladder_game(bot):
     # Load command line arguments
@@ -22,9 +22,6 @@ def run_ladder_game(bot):
     parser.add_argument("--GamePort", type=int, nargs="?", help="Game port")
     parser.add_argument("--StartPort", type=int, nargs="?", help="Start port")
     parser.add_argument("--LadderServer", type=str, nargs="?", help="Ladder server")
-    parser.add_argument("--ComputerOpponent", type=str, nargs="?", help="Computer opponent")
-    parser.add_argument("--ComputerRace", type=str, nargs="?", help="Computer race")
-    parser.add_argument("--ComputerDifficulty", type=str, nargs="?", help="Computer difficulty")
     parser.add_argument("--OpponentId", type=str, nargs="?", help="Opponent ID")
     args, unknown = parser.parse_known_args()
 
@@ -38,13 +35,6 @@ def run_ladder_game(bot):
 
     # Add opponent_id to the bot class (accessed through self.opponent_id)
     bot.ai.opponent_id = args.OpponentId
-
-    # Versus Computer doesn't work yet
-    computer_opponent = False
-    if args.ComputerOpponent:
-        computer_opponent = True
-        computer_race = args.ComputerRace
-        computer_difficulty = args.ComputerDifficulty
 
     # Port config
     ports = [lan_port + p for p in range(1, 6)]
