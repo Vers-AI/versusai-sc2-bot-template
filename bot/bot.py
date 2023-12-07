530d5cc168451742ebc9a55ad81a5592b9ce0068
Drekken <drekken.pownz@protonmail.com>
2023-12-06 20:57:21 -0500

    Revert "Update bot.py and run.py"
    This reverts commit 9afdfc068f0c01a711dea5903a971b8d64ce6e90.

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,9 @@
 from sc2.bot_ai import BotAI, Race
 from sc2.data import Result
-
+from sc2.ids.unit_typeid import UnitTypeId
 
 class CompetitiveBot(BotAI):
-    NAME: str = "CompetitiveBot"
+    NAME: str = "Zippy"
     """This bot's name"""
 
     RACE: Race = Race.Terran
@@ -27,7 +27,21 @@ class CompetitiveBot(BotAI):
         This code runs continually throughout the game
         Populate this function with whatever your bot should do!
         """
-        pass
+        for loop_cc in self.workers:
+            cc_list = self.townhalls(UnitTypeId.COMMANDCENTER).ready.idle
+            if self.can_afford(UnitTypeId.SCV) and self.workers.amount <= 15 and cc_list.exists:
+                self.townhalls.ready.random.train(UnitTypeId.SCV)
+            elif self.workers.amount == 15:
+                for worker in self.workers:
+                    worker.attack(self.enemy_start_locations[0])
+                    
+    async def on_end(self, result: Result):
+        """
+        This code runs once at the end of the game
+        Do things here after the game ends
+        """
+        print("Game ended.")
+
 
     async def on_end(self, result: Result):
         """
